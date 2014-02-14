160ca8ca63ea759f11d9520a825a903df1823239
satanabe1 <satoshi.watanabe.ssl@gmail.com>
2014-02-15 00:22:28 +0900

    フォントサイズに中身を埋めた そしてふぃんたふぇーすえ

--- src/jdoc2tex/layout/TexFontSize.java
+++ src/jdoc2tex/layout/TexFontSize.java
@@ -1,14 +1,17 @@
 package jdoc2tex.layout;
 
-public class TexFontSize {
+public interface TexFontSize {
 
-	final static String TINY = "";
-	final static String FOOTNOTESIZE = "";
-	final static String SCRIPTSIZE = "";
-	final static String SMALL = "";
-	final static String NORMALSIZE = "";
-	final static String LARGE1 = "";
-	final static String LARGE2 = "";
-	final static String LARGE3 = "";
-	
+	final static String TINY = "\\tiny";
+	final static String FOOTNOTESIZE = "\\footnotesize";
+	final static String SCRIPTSIZE = "\\scriptsize";
+	final static String SMALL = "\\small";
+	final static String NORMALSIZE = "\\nomalsize";
+	final static String LARGE1 = "large";
+	final static String LARGE2 = "Large";
+	final static String LARGE3 = "LARGE";
+	final static String HUGE1 = "huge";
+	final static String HUGE2 = "Huge";
+	final static String HUGE3 = "HUGE";
+
 }
